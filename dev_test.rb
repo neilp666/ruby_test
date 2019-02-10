3bbb49af5e81999193203a5b27d6a4f2c5416941
neil patel <neil@myindianweddingapp.com>
2019-02-10 18:46:58 +0000

    Created a new list to show unique page views from most to less

--- dev_test.rb
+++ dev_test.rb
@@ -6,4 +6,10 @@ class Webserver
   new_webserver.sort! {|x, y| x <=> y}
   puts new_webserver
 
+  new_webserver = Hash[new_webserver.uniq.map {|i| [i, new_webserver.count(i)]}]
+  new_webserver.sort{|a,b| b[1]<=>a[1]}.each { |elem| 
+  puts "#{elem[0]} has #{elem[1]} unique views"
+
+}
+
 end
