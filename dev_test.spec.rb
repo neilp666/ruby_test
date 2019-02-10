f9841c6e077eaf5c76ae030211cb58609f4a26ac
neil patel <neil@myindianweddingapp.com>
2019-02-10 18:52:32 +0000

    Added test to describe duplicate removal

--- dev_test.spec.rb
+++ dev_test.spec.rb
@@ -1,4 +1,8 @@
 require_relative 'dev_test'
 
-RSpec.describe Webserver do
+RSpec.describe 'Duplicate removal' do
+  it 'Removes duplicates from an array' do
+    arr = [1, 3, 4, 1, 4]
+    expect(arr.remove_duplicates).to eq[1, 4])
+  end
 end
