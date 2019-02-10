4c8b2636915dde55a4a3244ee2edee79b6e36a47
neil patel <neil@myindianweddingapp.com>
2019-02-10 18:44:26 +0000

    updated array list to show most page views to less pages views

--- dev_test.rb
+++ dev_test.rb
@@ -1,7 +1,9 @@
 class Webserver
+  new_webserver = Array.new
+  File.open('webserver.log').each { |line| new_webserver << line }
+  puts new_webserver
+
+  new_webserver.sort! {|x, y| x <=> y}
+  puts new_webserver
+
 end
-
-new_webserver = Array.new
-File.open('webserver.log').each { |line| new_webserver << line }
-
-puts new_webserver
