7c8fde0004e3ab3eea1fb6be4c08c0b75c8df97a
neil patel <neil@myindianweddingapp.com>
2019-02-10 19:04:33 +0000

    Added  2 more simple messages and end message

--- dev_test.rb
+++ dev_test.rb
@@ -28,12 +28,28 @@ class Webserver
   File.open('webserver.log').each { |line| new_webserver << line }
   puts new_webserver
 
+  puts "#2 Display webpages with most pages to less pages views."
+  puts "Press any key to continue or CTRL-C (^C) to quit"
+
+  gets.chomp
+
+  puts "Opening the file..."
+
   new_webserver.sort! {|x, y| x <=> y}
   puts new_webserver
+
+  puts "#3 Display webpages with most pages to less pages views."
+  puts "Press any key to continue or CTRL-C (^C) to quit"
+
+  gets.chomp
+
+  puts "Opening the file..."
 
   new_webserver = Hash[new_webserver.uniq.map {|i| [i, new_webserver.count(i)]}]
   new_webserver.sort{|a,b| b[1]<=>a[1]}.each { |elem| 
   puts "#{elem[0]} has #{elem[1]} unique views"
+
+  puts "The End! Goodbye"
 
 }
 
